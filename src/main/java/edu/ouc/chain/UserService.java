c0d0819154edbe40fe2abfa7051a7c2502adf233
TopGunViper <1449978622@qq.com>
2016-09-09 16:05:10 +0800

    新增测试用业务类，UserService

---
+++ src/main/java/edu/ouc/chain/UserService.java
@@ -0,0 +1,12 @@
+package edu.ouc.chain;
+
+/**
+ * 测试业务接口
+ * 
+ * @author wqx
+ *
+ */
+public interface UserService {
+	
+	public void insert(Object obj);
+}
---
+++ src/main/java/edu/ouc/chain/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package edu.ouc.chain;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public void insert(Object obj) {
+		System.out.println("insert user into db");
+	}
+}
