6102a800a3d126a72e1ed6edecaa58fab1110dcd
TopGunViper <1449978622@qq.com>
2016-12-14 23:46:10 +0800

    add ChannelHandler

--- src/main/java/edu/ouc/reactor/ChannelHandler.java
+++ src/main/java/edu/ouc/reactor/ChannelHandler.java
@@ -1,5 +1,11 @@
 package edu.ouc.reactor;
 
-public interface ChannelHandler {
 
+public interface ChannelHandler {
+	
+	void channelActive(NioChannel channel);
+	
+	void channelRead(NioChannel channel, Object msg) throws Exception;
+	
+	void exceptionCaught(NioChannel channel, Object exception) throws Exception;
 }
