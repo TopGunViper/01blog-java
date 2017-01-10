0f57a39305d46285c974f6241c59821e81885100
TopGunViper <1449978622@qq.com>
2017-01-10 22:22:03 +0800

    测试类 DummyBatchTask DummyWarningService

--- src/test/java/edu/ouc/async/event/DummyWarningService.java
+++ src/test/java/edu/ouc/async/event/DummyWarningService.java
@@ -2,7 +2,12 @@ package edu.ouc.async.event;
 
 public class DummyWarningService {
 	
-	public void dummyWarning(){
+	String taskName;
+	Object warningMsg;
+	
+	public void dummyWarning(BatchTask task, Object msg){
+		taskName = task.getTaskName();
+		warningMsg = msg;
 		/**
 		 * send email or messages.
 		 */
