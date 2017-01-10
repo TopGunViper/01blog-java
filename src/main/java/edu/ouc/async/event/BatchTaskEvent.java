fed77713eff3172ba04eeeca61a5328a80af042b
TopGunViper <1449978622@qq.com>
2017-01-10 22:20:35 +0800

    BatchTaskEvent implements Event

---
+++ src/main/java/edu/ouc/async/event/BatchTaskEvent.java
@@ -0,0 +1,21 @@
+package edu.ouc.async.event;
+
+public class BatchTaskEvent implements Event<BatchTask> {
+
+	private BatchTask task;
+	
+	private EventType type;
+	
+	public BatchTaskEvent(BatchTask task, EventType type){
+		this.task = task;
+		this.type = type;
+	}
+	@Override
+	public BatchTask getElement() {
+		return task;
+	}
+	@Override
+	public EventType getEventType() {
+		return type;
+	}
+}
