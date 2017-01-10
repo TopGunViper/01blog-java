ed59b3ec32cdaa306211da789f57220d05f61c23
TopGunViper <1449978622@qq.com>
2017-01-10 22:49:25 +0800

    删除timeout

--- src/main/java/edu/ouc/async/event/BatchTaskManager.java
+++ src/main/java/edu/ouc/async/event/BatchTaskManager.java
@@ -1,30 +1,18 @@
 package edu.ouc.async.event;
 
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 public class BatchTaskManager {
 
 	private BatchTask task;
 
-	private long timeout = -1;
-
-	private TimeUnit unit;
-	
 	private Listener<BatchTask> listener;
 
 	public BatchTaskManager task(BatchTask task){
-		return task(task, -1, null);
-	}
-	
-	public BatchTaskManager task(BatchTask task, long timeout, TimeUnit unit){
 		this.task = task;
-		this.timeout = timeout;
-		this.unit = unit;
 		return this;
 	}
+	
 	public BatchTaskManager listener(Listener<BatchTask> listener){
 		this.listener = listener;
 		return this;
